1adee93f06689ff0d34165183ad37d50ab2d3b91
elyrio <root@DrManhattan.pmsmj.local>
2017-07-26 16:39:19 -0300

    criando arquivos para acesso a ficha de Produtor.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\fichaPR;
+use Gate;
+use App\Http\Controllers\Controller;
+
+
+class HomeController extends Controller
+{
+    public function __construct(){
+    	//
+    }
+    public function listaFuncionario(){
+    	return"funcionarios";
+
+    }
+    public function viewFichaPR(){
+    	return"ok";
+
+    }
+}
