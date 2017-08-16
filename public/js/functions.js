49fb7c5f63d79a9df9505360e4872800f46c93e7
elyrio <root@DrManhattan.pmsmj.local>
2017-08-16 10:24:23 -0300

    Adicioando javascritp para grid.

---
+++ public/js/functions.js
@@ -0,0 +1,35 @@
+(function($) {
+
+  RemoveTableRow = function(handler) {
+    var tr = $(handler).closest('tr');
+
+    tr.fadeOut(400, function(){ 
+      tr.remove(); 
+    }); 
+
+    return false;
+  };
+  
+  AddTableRow = function() {
+      
+      var newRow = $("<tr>");
+      var cols = "";
+      
+      cols += '<td><input type="text" name="id"></td>';
+      cols += '<td><input type="text" name="nome"></td>';
+      cols += '<td><select name="cargo"><option value="gerente" name="gerente">Gerente</option><option value="Professor" name="Professor">Professor</option><option value="Programador" name="Programador">Programador</option></select></td>';
+      cols += '<td><input type="text" name="email"></td>';
+      cols += '<td><input type="text" name="cpf"></td>';
+      
+      cols += '<td class="actions">';
+      cols += '<button class="btn btn-large btn-danger" onclick="RemoveTableRow(this)" type="button">Remover</button>';
+      cols += '</td>';
+      
+      newRow.append(cols);
+      
+      $("#products-table").append(newRow);
+    
+      return false;
+  };
+  
+})(jQuery);
