e7d129436ef5839da3bd596a4e98d6d61a4185aa
elyrio <lyrio.nas@gmail.com>
2017-12-08 09:08:02 -0200

    Criando a tabela dos blocos dos Produtores.

---
+++ database/migrations/2017_12_08_104248_blocoProduto.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BlocoProduto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bloco_pr', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('numeracao')->index();
+            $table->string('quantidade')->default('0');
+            $table->string('date');
+            $table->string('autN');
+            $table->integer('idProdutor')->unsigned();
+            $table->foreign('idProdutor')->references('id')->on('ficha_p_rs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ficha_p_rs');
+    }
+}
