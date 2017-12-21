697af49cf6482d7a8ede096bb0be8980851f2ccc
elyrio <lyrio.nas@gmail.com>
2017-12-21 16:08:03 -0200

    Inserindo link e icones para página de estastística.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,20 +9,32 @@
                 </div>
 
                 <div class="panel-body">
-                    <div class="row">
-                      
 
-                      <div class="col-sm-6 col-md-4">
+                    <div class="row">
+
+                      <div class="col-sm-5 col-md-3">
                         <div class="thumbnail">
                           <a href="{{url('fichaProdRural')}}">
-                            <img src="icones/FPR-icon.png" alt="icone de usuário"/>
+                            <img src="icones/FPR-icon.png" alt="Ficha Produtor Rural"/>
                             <div class="caption">
                               <h4>Ficha Produtor Rural</h4>
                             </div>
                         </a>
                        </div>
                       </div>
-                      <div class="col-sm-6 col-md-4">
+
+                      <div class="col-sm-5 col-md-3">
+                        <div class="thumbnail">
+                          <a href="{{url('#')}}">
+                            <img src="icones/estatistica.png" alt="Dados estatísticos"/>
+                            <div class="caption">
+                              <h4>Dados Estatísticos</h4>
+                            </div>
+                        </a>
+                       </div>
+                      </div>
+
+                      <div class="col-sm-5 col-md-3">
                         <div class="thumbnail">
                           <a href="{{url('#')}}">
                             <img src="icones/Info-icon.png" alt="icone de usuário"/>
@@ -34,6 +46,7 @@
                       </div>
 
                     </div>
+
 
                     <div class="row">
                       <!-- <div class="col-sm-6 col-md-4">
@@ -49,7 +62,7 @@
                     </div> -->
 
                   </div>
-               </div>
+
            </div>
       </div>
    </div>
